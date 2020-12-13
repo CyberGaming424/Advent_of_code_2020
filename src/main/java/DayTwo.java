fbe9f713e1302bb2f4bdde6e9fde0d275e6f08af
CyberGaming424 <jlake624@gmail.com>
2020-12-13 18:01:00 -0500

    Fixed Day 2

--- src/main/java/DayTwo.java
+++ src/main/java/DayTwo.java
@@ -2,117 +2,74 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DayTwo {
 
-    private File day2;
-    private ArrayList<String> passwords;
-    private int validPasses = 0;
-    private int prevMin = 0;
-    private int prevMax = 0;
-    private char prevKey = ' ';
+    private String[] lines;
 
-    public DayTwo() {
+    public DayTwo(boolean partTwo) {
 
-        passwords = new ArrayList<>();
-        day2 = new File("D:\\Advent_Of_code\\src\\main\\resources\\day2.txt");
-
-        try{
-            Scanner in = new Scanner(day2);
-
-            while (in.hasNextLine()){
-                passwords.add(in.nextLine());
-            }
-
-        }catch (FileNotFoundException e){
+        try {
+            lines = Files.lines(Path.of("D:\\Advent_Of_code\\src\\main\\resources\\day2.txt")).toArray(String[]::new);
+        } catch (IOException e) {
             e.printStackTrace();
+            lines = new String[0];
         }
 
-        passwords.forEach(this::validPass);
+        if(!partTwo)
+            partOne(lines);
+        else
+            partTwo(lines);
 
-
-
-        System.out.println("Entries: " + passwords.size() + " Valid Passes: " + validPasses);
     }
 
-
-    void validPass(String passcode){
-        passcode = passcode.replaceAll("\\s+", "");
-        System.out.println(passcode);
-        int min = 0;
-        int max = 0;
-        int end = 0;
-        char key = ' ';
-        boolean valid = false;
-        int occurrences = 0;
-        ArrayList<Integer> minMax = new ArrayList<>();
-        ArrayList<Character> passChars = new ArrayList<>();
-        for (int i = 0; i < passcode.length(); i++) {
-            if(Character.isDigit(passcode.charAt(i)) && (!Character.isDigit(passcode.charAt(i+1)))){
-                minMax.add(Integer.parseInt(String.valueOf(passcode.charAt(i))));
-            }else if(Character.isDigit(passcode.charAt(i)) && Character.isDigit(passcode.charAt(i+1))){
-                minMax.add(Integer.parseInt(String.valueOf(passcode.charAt(i)) + String.valueOf(passcode.charAt(i+1))));
-            }
-
-            if(passcode.charAt(i) != '-' && passcode.charAt(i+1) == ':' && Character.isDigit(passcode.charAt(i-1))){
-                key = passcode.charAt(i);
-            }else if(passcode.charAt(i) == ':'){
-                end = i+1;
-                break;
-            }
-        }
-
-        for(;end < passcode.length(); end++){
-            passChars.add(passcode.charAt(end));
-        }
-
-
-        min = minMax.get(0);
-        for (int i:
-             minMax) {
-            if(min < i){
-                max = i;
-            }
-        }
-
-        for (char e :
-                passChars) {
-            if(e == key){
-                occurrences++;
-            }
-        }
-        System.out.println("key: " + key + " occurrences: " + occurrences + " min: " + min +
-                " PrevMin: " + prevMin + " max: " + max + " PrevMax: " + prevMax + " " + minMax +
-                " ValidPasses: " + validPasses);
-        if(occurrences != 0){
-            if(occurrences >= min && occurrences <= max){
-                validPasses++;
-                valid = true;
-            }
-        }
-
-        int oldOccurrences = 0;
-
-        if(prevKey != ' '){
-            for (char e:
-                 passChars) {
-                if(e == prevKey) {
-                    oldOccurrences++;
+    public static void partOne(String[] lines) {
+        int validCount = 0;
+        String regex = "(\\d+)-(\\d+)\\s+(\\w):\\s+(\\w+)";
+        for (String line : lines) {
+            Matcher m = Pattern.compile(regex).matcher(line);
+            if (m.find()) {
+                int min = Integer.parseInt(m.group(1));
+                int max = Integer.parseInt(m.group(2));
+                String req = m.group(3);
+                String pw = m.group(4);
+                Matcher findOccurrences = Pattern.compile(req).matcher(pw);
+                int count = 0;
+                while (findOccurrences.find()) {
+                    ++count;
+                }
+                if (count >= min && count <= max) {
+                    ++validCount;
                 }
             }
         }
+        System.out.println(validCount);
+    }
 
-        if(oldOccurrences != 0 && !valid){
-            if(oldOccurrences >= prevMin && oldOccurrences <= prevMax){
-                validPasses++;
+    public static void partTwo(String[] lines) {
+        int validCount = 0;
+        String regex = "(\\d+)-(\\d+)\\s+(\\w):\\s+(\\w+)";
+        for (String line : lines) {
+            Matcher m = Pattern.compile(regex).matcher(line);
+            if (m.find()) {
+                int pos1 = Integer.parseInt(m.group(1)) - 1;
+                int pos2 = Integer.parseInt(m.group(2)) - 1;
+                char req = m.group(3).charAt(0);
+                String pw = m.group(4);
+                if (pw.charAt(pos1) == req && pw.charAt(pos2) != req ||
+                        pw.charAt(pos2) == req && pw.charAt(pos1) != req) {
+                    ++validCount;
+                }
             }
         }
-
-        prevMin = min;
-        prevMax = max;
-        prevKey = key;
-
+        System.out.println(validCount);
     }
+
+
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //DayOne dayOne = new DayOne();
+        DayTwo dayTwo = new DayTwo(false);
+    }
+
+}
