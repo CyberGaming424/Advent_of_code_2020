f218f10f4a551c9c695fffc9e271d1d8dbcecca3
CyberGaming424 <jlake624@gmail.com>
2020-12-15 14:27:08 -0500

    Day 4 text input formatted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,28 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        if(args.length > 0) {
+            switch (Integer.parseInt(args[0])) {
+                case 1:
+                    new DayOne();
+                    break;
+                case 2:
+                    if (args[1] != null) {
+                        if (args[1].equalsIgnoreCase("true"))
+                            new DayTwo(true);
+                        else
+                            new DayTwo(false);
+                    } else
+                        System.out.println("Invalid arguments, argument 2 must be either false for part 1 or true for part 2!");
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Invalid argument!");
+                    System.out.println("Usage: -[Program 1 - 25]");
+            }
+        }
         //new DayOne();
         //new DayTwo(false);
         //new DayThree();
